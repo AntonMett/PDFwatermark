b3ed1e2c3ab8fbd76822ebe0099e99e28f21a090
Anton Mett <antonmett@gmail.com>
2021-05-20 22:04:39 +0300

    Working pdf watermarker.

--- PDFconverter.py
+++ PDFconverter.py
@@ -3,19 +3,27 @@ import sys
 
 pdf_file_list = sys.argv[1:]
 watermark = 'wtr.pdf'
-output = "watermarked.pdf"
+merger = PyPDF2.PdfFileMerger()
+writer = PyPDF2.PdfFileWriter()
+
+
 for file in pdf_file_list:
-    with open(file, 'rb') as fileinput:
-        pdf_file = PyPDF2.PdfFileReader(fileinput)
-        with open(watermark, 'rb') as filewatermark:
-            watermark_pdf = PyPDF2.PdfFileReader(filewatermark)
-            # print(pdf_file.getNumPages())
-            for page in range(pdf_file.getNumPages()):
-                #     print(page)
-                current_page = pdf_file.getPage(0)
-                first_page_watermark = watermark_pdf.getPage(0)
-                current_page.mergePage(first_page_watermark)
-                pdf_writer = PyPDF2.PdfFileWriter()
-                pdf_writer.addPage(current_page)
-                with open(output, 'wb')as file_output:
-                    pdf_writer.write(file_output)
+    merger.append(file)
+
+merger.write('super.pdf')
+
+
+with open('super.pdf', 'rb') as fileinput:
+    pdf_file = PyPDF2.PdfFileReader(fileinput)
+
+    with open(watermark, 'rb') as filewatermark:
+        watermark_pdf = PyPDF2.PdfFileReader(filewatermark)
+
+        for page in range(pdf_file.getNumPages()):
+            current_pdf_page = pdf_file.getPage(page)
+            first_page_watermark = watermark_pdf.getPage(0)
+            current_pdf_page.mergePage(first_page_watermark)
+            writer.addPage(current_pdf_page)
+
+            with open("watermarked.pdf", 'wb')as file_output:
+                writer.write(file_output)
